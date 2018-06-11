a2cfecd9d9d7130a04bf86e7dcabbd4bd64e9a7d
Jacob Jasper <jacob_jasper@hotmail.com>
2018-06-11 16:40:27 +0200

    day 1.1 week 2

---
+++ PROCESS.md
@@ -0,0 +1,15 @@
+# Week 1
+
+## 7 juni
+- Bezig geweest met het preprocessen van mijn data en het omzetten naar een JSON. Aangezien ik vastliep in week 3 daarmee, had ik daar nu ook weer moeite mee.
+- Ik kwam erachter tijdens het proprocessen dat mijn data soms verschilt in jaren en neit volledig overeenkomt.
+
+## 8 juni
+- Besloten om mijn visualisaties iets aan te passen. jaren lopen van 1990-2012 nu voor de heatmap, voor de scatter kan je kiezen tussen de jaren 1990-2010 met verschillende gaps van jaren ertussen (1990, 1995, 2000, 2005, 2006, 2010). en de line graph loopt van 1980 tot 2012 ook. Deze keuzes zijn gemaakt op basis van de data die beschikbaar is, kwam erachter dat mijn data niet overal hetzelfde is op de site waar ik het vandaag haal (7 juni).
+- Ook besloten de volgorde van de visualisaties iets aan te passen op aanraden van Sasha, dit kwam naar voren uit de feedback.
+- Dit ook aangepast in mijn proposal.
+
+# Week 2
+
+## 11 juni
+-
--- convert_into_JSON.py
+++ convert_into_JSON.py
@@ -1,5 +1,74 @@
 import csv
 import json
 
-json_data = [json.dumps(d) for d in csv.DictReader(open('csv_data/deaths_smoking.csv', 'r'))]
-print(json_data)
+# convert number of deaths by smoking into a JSON
+json_file = open('JSON_data/deaths_smoking.json', 'w')
+json_data = [json.dumps(d) for d in csv.DictReader(open('csv_data/deaths_smoking-(1990-2016).csv', 'r'))]
+
+json_file.write("[")
+for d in json_data:
+    json_file.write(d)
+    json_file.write(',\n')
+
+json_file.close()
+
+with open('JSON_data/deaths_smoking.json', 'rb+') as json_file:
+    json_file.seek(-2, 2)
+    json_file.truncate()
+
+with open('JSON_data/deaths_smoking.json', 'a') as json_file:
+    json_file.write("]")
+
+# convert share of cancer deaths attributed to tobacco into JSON
+json_file = open('JSON_data/share_cancer_deaths_tobacco.json', 'w')
+json_data = [json.dumps(d) for d in csv.DictReader(open('csv_data/share-of-cancer-deaths-attributed-to-tobacco-(1990-2016).csv', 'r'))]
+
+json_file.write("[")
+for d in json_data:
+    json_file.write(d)
+    json_file.write(',\n')
+
+json_file.close()
+
+with open('JSON_data/share_cancer_deaths_tobacco.json', 'rb+') as json_file:
+    json_file.seek(-2, 2)
+    json_file.truncate()
+
+with open('JSON_data/share_cancer_deaths_tobacco.json', 'a') as json_file:
+    json_file.write("]")
+
+# convert share of people who smoke everyday into a JSON
+json_file = open('JSON_data/share_smokers_everyday.json', 'w')
+json_data = [json.dumps(d) for d in csv.DictReader(open('csv_data/daily-smoking-prevalence-bounds-(1980-2012).csv', 'r'))]
+
+json_file.write("[")
+for d in json_data:
+    json_file.write(d)
+    json_file.write(',\n')
+
+json_file.close()
+
+with open('JSON_data/share_smokers_everyday.json', 'rb+') as json_file:
+    json_file.seek(-2, 2)
+    json_file.truncate()
+
+with open('JSON_data/share_smokers_everyday.json', 'a') as json_file:
+    json_file.write("]")
+
+# convert consumtion cigarets per smoker into JSON
+json_file = open('JSON_data/comsumption_cigarets_smokers.json', 'w')
+json_data = [json.dumps(d) for d in csv.DictReader(open('csv_data/consumption-per-smoker-per-day-(1980-2012).csv', 'r'))]
+
+json_file.write("[")
+for d in json_data:
+    json_file.write(d)
+    json_file.write(',\n')
+
+json_file.close()
+
+with open('JSON_data/comsumption_cigarets_smokers.json', 'rb+') as json_file:
+    json_file.seek(-2, 2)
+    json_file.truncate()
+
+with open('JSON_data/comsumption_cigarets_smokers.json', 'a') as json_file:
+    json_file.write("]")
